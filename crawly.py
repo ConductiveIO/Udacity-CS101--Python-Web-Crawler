f15398ec624333a907cb9fbef62eda97c5b0ce06
robby grodin <grodin.robby@gmail.com>
2012-09-13 23:14:52 -0700

    added get_all_links proc

--- crawly.py
+++ crawly.py
@@ -18,3 +18,13 @@ def get_next_target(page):
         end_quote = page.find('"', start_quote + 1)
         url = page[start_quote + 1:end_quote]
         return url, end_quote
+def get_all_links(page):
+	links = []
+	while True:
+		url, endpos = get_next_target(page)
+		if url:
+			links.append(url)
+			page = page[endpos:]
+		else:
+			break
+	return links
